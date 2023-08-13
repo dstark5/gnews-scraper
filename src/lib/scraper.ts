f52d57dd17f09b8a73dc2c33826e2666ddc9fffa
Harikrishna-28 <harikrishnac2002@gmail.com>
2023-08-13 13:21:37 +0530

    add puppeteer launch arguements

--- src/lib/scraper.ts
+++ src/lib/scraper.ts
@@ -1,54 +1,82 @@
-import puppeteer from 'puppeteer';
-import serializeURL from './serializeUrl.js';
-import type {Options,NewsData,ElementType} from '../types/types';
+import puppeteer from "puppeteer";
+import serializeURL from "./serializeUrl.js";
+import type { Options, NewsData, ElementType } from "../types/types";
 
+async function scraper(config: Options): Promise<NewsData[]> {
+  const defaultArgs = [
+    "--incognito",
+    "--no-sandbox",
+    "--single-process",
+    "--no-zygote",
+    "--disable-dev-shm-usage",
+    "--disable-setuid-sandbox",
+  ];
+  if (config?.proxy?.host && config?.proxy?.port) {
+    defaultArgs.push(
+      `--proxy-server=http=${config.proxy.host}:${config.proxy.port}`
+    );
+  }
+  // chack for args in the config and go with the default arguements if there are not any args
+  const browser = await puppeteer.launch({
+    args: config?.args ? config.args : defaultArgs,
+  });
+  const page = await browser.newPage();
 
-async function scraper(config:Options):Promise<NewsData[]>{
-    const defaultArgs = ["--incognito","--no-sandbox","--single-process","--no-zygote",'--disable-dev-shm-usage',"--disable-setuid-sandbox"]
-    if(config?.proxy?.host && config?.proxy?.port){
-        defaultArgs.push(`--proxy-server=http=${config.proxy.host}:${config.proxy.port}`)
-    }
-    const browser = await puppeteer.launch({args: defaultArgs})
-    const page = await browser.newPage();
+  const url: string = serializeURL(config);
+  await page.goto(url, { waitUntil: "domcontentloaded" });
 
-    const url:string=serializeURL(config);
-    await page.goto(url,{'waitUntil':'domcontentloaded'});
-  
-    await page.setViewport({width: 1080, height: 1024});
+  await page.setViewport({ width: 1080, height: 1024 });
 
-    const pageUrl:string=await page.url();
+  const pageUrl: string = await page.url();
 
-    if(pageUrl.startsWith('https://consent.google.com')){
-       await page.click('button[class="VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc LQeN7 Nc7WLe"]');
-       await page.waitForNavigation({waitUntil: 'networkidle2'});
-    }
-  
-    var newsData:NewsData[]=await page.evaluate(()=>{
-      
-      var data:NewsData[]=[];
-  
-      var divNode= document.querySelectorAll('div[class="NiLAwe y6IFtc R7GTQ keNKEd j7vNaf nID9nc"]');
-  
-      divNode.forEach((node)=>{
-          var title:ElementType=node.querySelector('h3[class="ipQwMb ekueJc RD0gLb"]>a')?.textContent;
-          var articleUrl:ElementType=node.querySelector('a[class="VDXfz"]')?.getAttribute('href')?.replace('.','https://news.google.com');
-          var sourceName:ElementType=node.querySelector('a[class="wEwyrc"]')?.textContent;
-          var imageUrl:ElementType=node.querySelector('img[class="tvs3Id QwxBBf"]')?.getAttribute('src');
-          var time:ElementType=node.querySelector('time[class="WW6dff uQIVzc Sksgp slhocf"]')?.textContent;
-  
-          if(title&&articleUrl&&sourceName&&imageUrl&&time){
-            data.push({
-              title,articleUrl,sourceName,imageUrl,time
-            })
-          };
-      })
-  
-      return data;
-    })
-  
-    await browser.close();
-   
-    return newsData;
+  if (pageUrl.startsWith("https://consent.google.com")) {
+    await page.click(
+      'button[class="VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc LQeN7 Nc7WLe"]'
+    );
+    await page.waitForNavigation({ waitUntil: "networkidle2" });
+  }
+
+  var newsData: NewsData[] = await page.evaluate(() => {
+    var data: NewsData[] = [];
+
+    var divNode = document.querySelectorAll(
+      'div[class="NiLAwe y6IFtc R7GTQ keNKEd j7vNaf nID9nc"]'
+    );
+
+    divNode.forEach((node) => {
+      var title: ElementType = node.querySelector(
+        'h3[class="ipQwMb ekueJc RD0gLb"]>a'
+      )?.textContent;
+      var articleUrl: ElementType = node
+        .querySelector('a[class="VDXfz"]')
+        ?.getAttribute("href")
+        ?.replace(".", "https://news.google.com");
+      var sourceName: ElementType =
+        node.querySelector('a[class="wEwyrc"]')?.textContent;
+      var imageUrl: ElementType = node
+        .querySelector('img[class="tvs3Id QwxBBf"]')
+        ?.getAttribute("src");
+      var time: ElementType = node.querySelector(
+        'time[class="WW6dff uQIVzc Sksgp slhocf"]'
+      )?.textContent;
+
+      if (title && articleUrl && sourceName && imageUrl && time) {
+        data.push({
+          title,
+          articleUrl,
+          sourceName,
+          imageUrl,
+          time,
+        });
+      }
+    });
+
+    return data;
+  });
+
+  await browser.close();
+
+  return newsData;
 }
 
 export default scraper;
